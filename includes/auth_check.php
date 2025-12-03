acf3acc832520fa171a337c44befd3bee0eebec1
handayaninury888-wq <handayaninury888@gmail.com>
2025-12-03 11:25:46 +0700

    Delete includes/auth_check.php

--- includes/auth_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-// admin/includes/auth_check.php
-session_start();
-// Pastikan user sudah login
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header('Location: login.php');
-    exit;
-}
-?>
