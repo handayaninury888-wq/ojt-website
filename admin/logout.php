8d3d757d6bc876b7720c2c222ebe86d6715ad0a3
handayaninury888-wq <handayaninury888@gmail.com>
2025-12-08 15:11:56 +0700

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,14 @@
 <?php
+// Mulai sesi
 session_start();
+
+// Hapus semua data sesi
+$_SESSION = array();
+
+// Hancurkan sesi
 session_destroy();
+
+// Redirect ke halaman utama (index.php di root)
 header('Location: ../index.php');
 exit;
 ?>
